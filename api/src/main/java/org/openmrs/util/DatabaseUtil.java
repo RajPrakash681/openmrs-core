a4860f0a6bba69ee381317a31148d65ba3538133
Raj-bytecommandor <rjnt452@gmail.com>
2026-02-24 23:44:31 +0530

    Fix: Reduce cognitive complexity and sanitize error log in loadDatabaseDriver
    - Remove unnecessary log level guards (isDebugEnabled, isInfoEnabled) to reduce cognitive complexity from 16 to 14 (below SonarQube threshold of 15)
    - Add sanitization to error log for unauthorized driver attempts
    - SLF4J parameterized logging is already efficient without guards

--- api/src/main/java/org/openmrs/util/DatabaseUtil.java
+++ api/src/main/java/org/openmrs/util/DatabaseUtil.java
@@ -73,9 +73,7 @@ public class DatabaseUtil {
 				throw new IllegalArgumentException("Database driver '" + connectionDriver + "' is not an allowed driver.");
 			}
 			Class.forName(connectionDriver);
-			if (log.isDebugEnabled()) {
-				log.debug("set user defined Database driver class: {}", OpenmrsUtil.sanitizeForLogging(connectionDriver));
-			}
+			log.debug("set user defined Database driver class: {}", OpenmrsUtil.sanitizeForLogging(connectionDriver));
 		} else {
 			if (connectionUrl.contains("jdbc:mysql")) {
 				Class.forName(MYSQL_DRIVER);
@@ -103,9 +101,7 @@ public class DatabaseUtil {
 				connectionDriver = H2_DRIVER;
 			}
 		}
-		if (log.isInfoEnabled()) {
-			log.info("Set database driver class as {}", OpenmrsUtil.sanitizeForLogging(connectionDriver));
-		}
+		log.info("Set database driver class as {}", OpenmrsUtil.sanitizeForLogging(connectionDriver));
 		return connectionDriver;
 	}
 	
